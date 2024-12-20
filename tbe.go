d8e2837382c46ed897e0fb492cffbf02bf15d4c6
Peter Admilson Ramaldes <peter.ramaldes@gmail.com>
2024-12-20 16:09:23 -0300

    Make it easy to add new test cases

--- tbe.go
+++ tbe.go
@@ -1,5 +1,17 @@
 package tbe
 
-func Foo() error {
-	return nil
+import (
+	"fmt"
+	"slices"
+)
+
+func Foo(input string) (string, error) {
+	allowed := []string{"foo", "bar", "baz"}
+
+	if !slices.Contains(allowed, input) {
+		err := fmt.Errorf("%v is not allowed", input)
+		return "", err
+	}
+
+	return input, nil
 }
--- tbe_test.go
+++ tbe_test.go
@@ -1,13 +1,53 @@
 package tbe_test
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/peterramaldes/tbe"
 )
 
-func TestFoo(t *testing.T) {
-	if err := tbe.Foo(); err != nil {
-		t.Fatal(err)
+var stst = []struct {
+	input  string
+	output string
+}{
+	{"foo", "foo"},
+	{"bar", "bar"},
+	{"baz", "baz"},
+}
+
+func TestSuccessFoo(t *testing.T) {
+	for _, e := range stst {
+		result, err := tbe.Foo(e.input)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if result != e.output {
+			t.Errorf("Foo(%v) = %v, want %v", e.input, result, e.output)
+		}
+	}
+}
+
+var ftst = []struct {
+	input string
+}{
+	{"ti"},
+	{"boo"},
+	{"baa"},
+	{"iii"},
+}
+
+func TestFailedFoo(t *testing.T) {
+	for _, e := range ftst {
+		_, err := tbe.Foo(e.input)
+		if err == nil {
+			t.Fatal("expecting an error for not allowed word")
+		}
+
+		errMsg := fmt.Sprintf("%s is not allowed", e.input)
+		if err.Error() != errMsg {
+			t.Fatalf("expected %s but %s", errMsg, err)
+		}
 	}
 }
