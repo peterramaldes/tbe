8f2dfd0e4ce7e6753c245f36f004148ff681456f
Peter Admilson Ramaldes <peter.ramaldes@gmail.com>
2024-12-20 16:51:55 -0300

    Write exhaustive tests

--- tbe_test.go
+++ tbe_test.go
@@ -1,11 +1,43 @@
 package tbe_test
 
 import (
+	"crypto/rand"
+	"encoding/base64"
 	"fmt"
 	"testing"
 
 	"github.com/peterramaldes/tbe"
 )
+
+func TestExhaustive(t *testing.T) {
+	data := make([]string, 999)
+	for i := range data {
+		data[i] = generateRandomString(10)
+	}
+
+	for i := 0; i < len(data); i++ {
+		v := data[i]
+		_, err := tbe.Foo(v)
+		if err == nil {
+			t.Fatalf("expected an error but none error received")
+		}
+
+		errMsg := fmt.Sprintf("%s is not allowed", v)
+		if err.Error() != errMsg {
+			t.Fatalf("expected %s but %s", errMsg, err)
+		}
+	}
+
+}
+
+func generateRandomString(length int) string {
+	b := make([]byte, length)
+	_, err := rand.Read(b)
+	if err != nil {
+		panic(err)
+	}
+	return base64.StdEncoding.EncodeToString(b)
+}
 
 var stst = []struct {
 	input  string
